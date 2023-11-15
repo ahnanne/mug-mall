808ad423b7b4869af07c507033c6c116531b9288
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-11-15 22:43:53 +0900

    [temp] test product api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
-const MainPage = () => (
-  <>
-    <h2>메인 페이지</h2>
-    <button type="button">test</button>
-  </>
-);
+import { useGetProductCategories } from '@/hooks/queries/product';
+
+const MainPage = () => {
+  const { data, isFetching, isSuccess, isError } = useGetProductCategories();
+
+  return (
+    <>
+      <h2>메인 페이지</h2>
+      <div>
+        <h3>Check our product categories!</h3>
+        {isFetching && <p>loading...</p>}
+        {isSuccess && (
+          <ul>
+            {(data?.data ?? []).map((category, idx) => (
+              <li key={idx}>{category}</li>
+            ))}
+          </ul>
+        )}
+        {isError && <p>Something went wrong..</p>}
+      </div>
+      <button type="button">test</button>
+    </>
+  );
+};
 
 export default MainPage;
