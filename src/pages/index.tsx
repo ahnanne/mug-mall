c2f78f2264eec96d9c566fed1e8bf8f67680de63
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-12-03 00:56:44 +0900

    [feat] add product list page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { useGetProductCategories } from '@/hooks/queries/product';
 
 const MainPage = () => {
@@ -8,11 +10,13 @@ const MainPage = () => {
       <h2>메인 페이지</h2>
       <div>
         <h3>Check our product categories!</h3>
-        {isFetching && <p>loading...</p>}
+        {isFetching && <p aria-busy="true">loading...</p>}
         {isSuccess && (
           <ul>
             {(data?.data ?? []).map((category, idx) => (
-              <li key={idx}>{category}</li>
+              <li key={idx}>
+                <Link to={`/products?category=${category}`}>{category}</Link>
+              </li>
             ))}
           </ul>
         )}
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,3 +1,76 @@
-const ProductListPage = () => <h2>상품 목록</h2>;
+import { useState, useMemo, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { parse } from 'qs';
+
+import { useGetCategoryProducts } from '@/hooks/queries/product';
+
+import Item from '@/components/scoped/products/Item';
+
+const ProductListPage = () => {
+  const [category, setCategory] = useState('');
+  const { search } = useLocation();
+
+  const isCategorySet = useMemo(() => Boolean(category), [category]);
+
+  const { data, refetch, isFetching, isSuccess, isError } =
+    useGetCategoryProducts(
+      {
+        params: { category },
+        query: { limit: 20 },
+      },
+      isCategorySet
+    );
+
+  useEffect(() => {
+    const queries = parse(search, {
+      ignoreQueryPrefix: true, // to remove '?' on parse result
+    });
+
+    setCategory((queries.category as string) ?? '');
+  }, []);
+
+  useEffect(() => {
+    if (!category) {
+      return;
+    }
+
+    refetch();
+  }, [category]);
+
+  return (
+    <>
+      <h2>상품 목록 - {category}</h2>
+      <div>
+        {isFetching && <p aria-busy="true">loading...</p>}
+        {!isFetching && (
+          <>
+            {isSuccess && (
+              <ul
+                style={{
+                  display: 'flex',
+                  flexFlow: 'row wrap',
+                  justifyContent: 'center',
+                }}
+              >
+                {(data?.data ?? []).map((product, idx) => (
+                  <li
+                    key={idx}
+                    style={{
+                      listStyle: 'none',
+                      margin: '0 9px',
+                    }}
+                  >
+                    <Item {...product} />
+                  </li>
+                ))}
+              </ul>
+            )}
+            {isError && <p>Something went wrong..</p>}
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default ProductListPage;
