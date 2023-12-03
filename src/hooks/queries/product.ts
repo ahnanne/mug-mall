1866077615eb6a584fbb1169a1a991c1982be752
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-12-03 14:43:37 +0900

    [feat] add product detail fetching query

--- src/hooks/queries/product.ts
+++ src/hooks/queries/product.ts
@@ -2,7 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import queryKeys from '@/api/queryKeys';
 
 import productApi from '@/api/product';
-import { GetCategoryProductsParamsAndQuery } from '@/api/product/types';
+import {
+  GetCategoryProductsParamsAndQuery,
+  GetProductParams,
+} from '@/api/product/types';
 
 export const useGetProductCategories = () => {
   const result = useQuery({
@@ -21,6 +24,16 @@ export const useGetCategoryProducts = (
   const result = useQuery({
     queryKey: [queryKeys.CATEGORY_PRODUCTS],
     queryFn: () => productApi.getCategoryProducts(options),
+    enabled,
+  });
+
+  return result;
+};
+
+export const useGetProduct = (options: GetProductParams, enabled: boolean) => {
+  const result = useQuery({
+    queryKey: [queryKeys.PRODUCT],
+    queryFn: () => productApi.getProduct(options),
     enabled,
   });
 
